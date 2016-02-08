36b21b50e3cd5ecf302d32e2ee1d644992ceeebc
Yoshiyuki Yamane <konoiz@konoiz-macbook.local>
2016-02-08 16:05:07 +0900

    Initial Commit

---
+++ opencv_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  opencv_test
+//
+//  Created by Yoshiyuki Yamane on 2016/02/08.
+//  Copyright © 2016年 Yoshiyuki Yamane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
